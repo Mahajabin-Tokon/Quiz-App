5044cf2fcc1cfadd15b47dd86af7d256d2a73d36
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-02-09 10:48:04 -0700

    end

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const questions = [
         question: "Which is the smallest country in the world?",
         answers: [
             { text: "Vatican City", correct: "true"},
-            { text: "Bhutan", correct: "true"},
+            { text: "Bhutan", correct: "false"},
             { text: "Nepal", correct: "false"},
             { text: "Sri Lanka", correct: "false"}
         ]
@@ -23,7 +23,7 @@ const questions = [
             { text: "Kalahari", correct: "false"},
             { text: "Gobi", correct: "true"},
             { text: "Sahara", correct: "false"},
-            { text: "Antarctica", correct: "true"}
+            { text: "Antarctica", correct: "false"}
         ]
     },
     {
@@ -39,7 +39,7 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 
@@ -47,6 +47,7 @@ let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz() {
+    resetState();
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
@@ -54,17 +55,71 @@ function startQuiz() {
 }
 
 function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    console.log(questionNo);
+    
     questionElement.innerHTML = questionNo +". "+ currentQuestion.question
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
     })
 }
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct")
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Try again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
 
 startQuiz();
